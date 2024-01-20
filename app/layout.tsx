08993319e9ab6a645e97aa3a243e9b7deba0ab96
Aqib Shabir <aqibfaisalshabir@gmail.com>
2024-01-20 19:15:36 +0000

    created intro section

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,9 +17,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-gray-50 text-gray-950 relative h-[5000px]`}>
-        <div className="bg-[#fbe2e3] z-10 absolute top-[-6rem] right-[-11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
-        <div className="bg-[#dbd7fb] z-10 absolute top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left[-28rem] xl:left-[-5rem] 2xl:left-[-5rem]"></div>
+      
+      <body className={`${inter.className} bg-gray-50 text-gray-950 relative h-[5000px] pt-24 sm:pt-28`}>
+
+        <div className="bg-[#fbe2e3] -z-10 absolute top-[-6rem] right-[-11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
+
+        <div className="bg-[#dbd7fb] -z-10 absolute top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left[-28rem] xl:left-[-5rem] 2xl:left-[-5rem]"></div>
+
         <Header />
         {children}
       </body>
