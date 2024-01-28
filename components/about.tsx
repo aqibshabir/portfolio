d2ef63b6a6d7d66326480adcc5bc5b6f217345b0
Aqib Shabir <aqibfaisalshabir@gmail.com>
2024-01-28 21:25:11 +0000

    changed project images

--- components/about.tsx
+++ components/about.tsx
@@ -18,14 +18,14 @@ export default function About() {
       id="about"
     >
       <SectionHeading>About me</SectionHeading>
-      <div className="mb-10 text-sm sm:text-lg ">
+      <div className="mb-10 text-sm sm:text-lg tracking-wide">
         <p className="mb-4"><span className="">My first encounter with programming</span> began at the start of the pandemic. As an avid streamer on Twitch, I wanted to create a bespoke chat that could be displayed alongside my stream. I couldn&apos;t find any information on this apart from an obsecure video in Portguese &ndash; only problem? <a target="_blank" href="https://thebackpackerintern.com/_wpx/wp-content/uploads/2015/01/Schermafbeelding-2015-01-19-om-13.31.25.png" className="font-semibold hover:underline hover:text-[#9884fc] transition-all">I don&apos;t speak Portguese&#33;</a> Naturally, my curiosity got the better of me and a few days later of tinkering around with the CSS styling, I had an amazing looking chat &ndash; that I'm still proud of to this day.
         </p>
         <p className="mb-4">
           Since then, I&apos;ve had an itch that wasn&apos;t quite scratched. I knew that I wanted to pursue a career in software development, so I took part in a 12 week bootcamp offered by <a href="https://www.thejump.tech/" target="_blank" className="font-semibold hover:text-[#9884fc] hover:underline transition-all">The Jump Digital School</a> &ndash; they've helped me to understand the core concepts of <span className="underline">Javascript, React, Express, Node and MongoDB</span> - <span className="italic">and much more&#33;</span>
         </p>
         <p className="mb-4">
-      When I&apos;m not coding, I&apos;m usually playing video games, <span className="italic">cooking a killer omlette for breakfast</span> and travelling to different parts of the world. 
+      When I&apos;m not coding, I&apos;m usually playing video games, <span className="italic">cooking a killer omlette for breakfast</span> and travelling to different parts of the world.
       </p>
       <p className="mb-4">
       <a href="#contact" className="font-semibold hover:text-[#9884fc] hover:underline transition-all">If you're an employer - give this a click and say hi&#33;</a>
