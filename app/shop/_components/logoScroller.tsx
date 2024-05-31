b249dd8bc8634068bb97809fe6c5acb21e315dba
Aqib Shabir <aqibfaisalshabir@gmail.com>
2024-05-31 21:06:49 +0100

    card elements rough mobile UI (shop page)

--- app/shop/_components/logoScroller.tsx
+++ app/shop/_components/logoScroller.tsx
@@ -6,7 +6,7 @@ function LogoScroller() {
   return (
     <>
       <div
-        className="group ml-auto mr-auto flex justify-center max-w-[90%] sm:-w-[90%] md:-w-[90%]  lg:max-w-[1400px] h-[200px] overflow-hidden mb-10"
+        className="group ml-auto mr-auto flex justify-center max-w-[90%] sm:-w-[90%] md:-w-[90%]  lg:max-w-[1400px] overflow-hidden"
         style={{
           WebkitMask:
             "linear-gradient(90deg, transparent, white 20%, white 50%, transparent)",
@@ -14,7 +14,7 @@ function LogoScroller() {
         }}
       >
         <div className="flex justify-center animate-loop-scroll ease-in-out group-hover:paused ml-[800px] md:ml-[1500px]">
-          <ul className="flex p-[2rem] sm:p-[3rem] md:p-[4rem] gap-[2.5rem] sm:gap-[4rem] md:gap-[6rem] text-4xl sm:text-6xl md:text-8xl">
+          <ul className="flex p-[2rem] sm:p-[3rem] md:p-[4rem] gap-[2.5rem] sm:gap-[4rem] md:gap-[6rem] text-4xl sm:text-6xl md:text-8xl h-[10px]">
             <li>{skillsData[2].icon}</li>
             <li>{skillsData[4].icon}</li>
             <li>{skillsData[5].icon}</li>
--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,3 +1,5 @@
+"use-client"
+
 import React from "react";
 import Header from "./_components/header";
 import Video from "./_components/video";
@@ -5,11 +7,21 @@ import LogoScroller from "./_components/logoScroller";
 
 export default function Page() {
   return (
-    <>
+    <div className="">
       <Header />
       <h1 className="text-center">Under Maintainance!</h1>
       <Video />
       <LogoScroller />
-    </>
+      <div className="flex-col">
+        <div className="mt-10 h-[180px] w-[320px] dark:bg-transparent/40 hover:dark:bg-transparent/90 bg-white/60  border border-gray-200 dark:border-black rounded-3xl mr-auto ml-auto mb-8 hover:bg-white/100 hover:border-1.5 text-sm p-4 transition hover:scale-105">
+          <p className="font-bold">default</p>
+          <p className="text-gray-400">Lorem ipsum dolor sit amet consectetur!</p>
+          <p>hi</p>
+            </div>
+        <div className="h-[350px] w-[320px] dark:bg-transparent/40 hover:dark:bg-transparent/90 bg-white/60  border border-gray-200 dark:border-black rounded-3xl mr-auto ml-auto mb-8 hover:bg-white/100 hover:border-1.5 transition hover:scale-105"></div>
+        <div className="h-[180px] w-[320px] dark:bg-transparent/40 hover:dark:bg-transparent/90 bg-white/60  border border-gray-200 dark:border-black rounded-3xl mr-auto ml-auto mb-8 hover:bg-white/100 hover:border-1.5 transition hover:scale-105"></div>
+      </div>
+      <div className="h-[1000px]"></div>
+    </div>
   );
 }
