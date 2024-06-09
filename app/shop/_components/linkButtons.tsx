c6d7a5c647c9bcc0a2e2e0c54e8c973a41023075
Aqib Shabir <aqibfaisalshabir@gmail.com>
2024-06-09 20:38:51 +0100

    changes in link button (responsiveness)

--- app/shop/_components/linkButtons.tsx
+++ app/shop/_components/linkButtons.tsx
@@ -3,7 +3,7 @@ import { FaExternalLinkAlt, FaGithub } from "react-icons/fa";
 
 function LinkButtons() {
   return (
-    <div className="mt-8 w-full h-full flex flex-wrap justify-evenly mb-10 gap-6 px-3">
+    <div className="mt-8 w-full h-full flex flex-wrap justify-evenly mb-10 gap-6 px-3 max-w-[1200px] mx-auto">
       <a href="https://github.com/aqibshabir/shopping-comparison" target="_blank" className="flex justify-center items-center gap-2 dark:bg-white/90 dark:text-black w-[300px] h-[60px] sm:px-12 sm:py-6 rounded-xl dark:hover:bg-white bg-black text-white hover:bg-transparent/80 capitalize cursor-pointer font-semibold">
         github repository
         <FaGithub size={25} />
