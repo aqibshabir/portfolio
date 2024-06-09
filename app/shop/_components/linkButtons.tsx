e5e9f44220df0806b1dde2decfeb11c2ce16449b
Aqib Shabir <aqibfaisalshabir@gmail.com>
2024-06-09 20:11:20 +0100

    implemented carousel

--- app/shop/_components/linkButtons.tsx
+++ app/shop/_components/linkButtons.tsx
@@ -3,15 +3,15 @@ import { FaExternalLinkAlt, FaGithub } from "react-icons/fa";
 
 function LinkButtons() {
   return (
-    <div className="w-full h-full flex flex-wrap justify-evenly mb-10 gap-6">
-      <button className="flex justify-center items-center gap-2 dark:bg-white/70 dark:text-black w-[300px] h-[60px] sm:px-12 sm:py-6 rounded-xl dark:hover:bg-white bg-black text-white hover:bg-transparent/80">
-        github repo
-        <FaGithub size={20} />
-      </button>
-      <button className="flex justify-center items-center gap-2 dark:bg-transparent/80 w-[300px] h-[60px] sm:px-12 sm:py-6 rounded-xl">
+    <div className="mt-8 w-full h-full flex flex-wrap justify-evenly mb-10 gap-6 px-3">
+      <a href="https://github.com/aqibshabir/shopping-comparison" target="_blank" className="flex justify-center items-center gap-2 dark:bg-white/90 dark:text-black w-[300px] h-[60px] sm:px-12 sm:py-6 rounded-xl dark:hover:bg-white bg-black text-white hover:bg-transparent/80 capitalize cursor-pointer font-semibold">
+        github repository
+        <FaGithub size={25} />
+      </a>
+      <a className="flex justify-center items-center gap-2 dark:bg-transparent/40 dark:hover:bg-transparent/20 w-[300px] h-[60px] sm:px-12 sm:py-6 rounded-xl capitalize cursor-not-allowed hover:text-gray-400 bg-white hover:bg-white/65 font-semibold">
         live link
-        <FaExternalLinkAlt size={20} />
-      </button>
+        <FaExternalLinkAlt size={16} />
+      </a>
     </div>
   );
 }
