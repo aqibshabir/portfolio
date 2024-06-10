ba0eadee089f57958c0a0253a5ba71d289543c9a
Aqib Shabir <aqibfaisalshabir@gmail.com>
2024-06-10 18:01:27 +0100

    working on stream page

--- app/stream/_components/cards.tsx
+++ app/stream/_components/cards.tsx
@@ -4,7 +4,6 @@ export default function Cards() {
   return (
     <div className="sm:flex sm:justify-center sm:items-center lg:m-4 sm:gap-8 mt-10 md:mt-16 lg:mt-20 xl:mt-20">
       <div className="hidden sm:block mb-8 h-[600px] w-[320px] md:h-[750px] md:w-[405px] lg:h-[770px] lg:w-[550px] xl:w-[800px] p-4 text-gray-600 hover:text-gray-950  bg-white/20  border border-black/[0.1] rounded-2xl hover:bg-white dark:bg-opacity-5 dark:border-black/80 dark:bg-black/50 dark:hover:bg-black/10 dark:text-white/80 dark:hover:text-white">
-
       </div>
       <div className="flex-col">
         <div className="sm:hidden h-[600px] w-[320px] mr-auto ml-auto sm:ml-0 sm:mr-0 mb-8 sm:mb-0 p-4 text-gray-600 hover:text-gray-950  bg-white/20  border border-black/[0.1] rounded-2xl hover:bg-white dark:bg-opacity-5 dark:border-black/80 dark:bg-black/50 dark:hover:bg-black/10 dark:text-white/80 dark:hover:text-white">
