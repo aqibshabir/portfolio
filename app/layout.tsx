cbb9c583d645e9b0ee524b68fe2536c3b42a5be3
aqibshabir <aqibfaisalshabir@gmail.com>
2024-06-16 14:25:59 +0100

    got rid of the theme flicker for nextjs

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { Toaster } from "sonner";
 import Footer from "@/components/footer";
 import ThemeSwitch from "@/components/theme-switch";
 import ThemeContextProvider from "@/context/theme-context";
+import { Providers } from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -34,7 +35,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="!scroll-smooth">
+    <html
+      lang="en"
+      className={"!scroll-smooth"}
+      suppressHydrationWarning={true}
+    >
       <body
         className={`${inter.className} bg-gray-50 text-gray-950 relative pt-24 sm:pt-28 overflow-x-hidden dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
       >
@@ -42,14 +47,16 @@ export default function RootLayout({
         <div className="bg-[#dbd7fb] -z-10 absolute top-[-1rem] left-[0] h-[31.25rem] w-1/2 sm:w-[68.75rem] md:w-[68.75rem] lg:w-[90.75rem] 2xl:w-[120rem] rounded-full blur-[10rem] md:left-[-30rem] lg:left-[-40rem] dark:bg-[#211d41]"></div>
         <div className="bg-[#fbe2e3] -z-10 absolute bottom-[0rem] right-[0] h-[31.25rem] w-1/2 rounded-full blur-[10rem] sm:w-[68.75rem] lg:w-[90.75rem] 2xl:w-[150rem] dark:bg-[#2f273f]"></div>
         <div className="bg-[#dbd7fb] -z-10 absolute bottom-[0rem] left-[0] h-[31.25rem] w-1/2 sm:w-[68.75rem] md:w-[68.75rem] lg:w-[90.75rem] 2xl:w-[120rem] rounded-full blur-[10rem] md:left-[-30rem] lg:left-[-40rem] dark:bg-[#211d41]"></div>
-        <ThemeContextProvider>
-          <ActiveSectionContextProvider>
-            {children}
-            <Toaster position="bottom-right" />
-            <Footer />
-            <ThemeSwitch />
-          </ActiveSectionContextProvider>
-        </ThemeContextProvider>
+        <Providers>
+          <ThemeContextProvider>
+            <ActiveSectionContextProvider>
+              {children}
+              <Toaster position="bottom-right" />
+              <Footer />
+              <ThemeSwitch />
+            </ActiveSectionContextProvider>
+          </ThemeContextProvider>
+        </Providers>
       </body>
     </html>
   );
--- components/intro.tsx
+++ components/intro.tsx
@@ -18,11 +18,11 @@ export default function Intro() {
     return (
       <div className="h-[1000px]">
         <div>
-          <h1 className=" mt-40 mb-20 text-6xl text-center text-transparent/15 dark:text-white">
+          <h1 className=" mt-40 mb-10 text-6xl text-center font-black text-gray-800/40 dark:text-white/70">
             Loading
           </h1>
         </div>
-        <div className="flex text-transparent/15 dark:text-white">
+        <div className="flex text-gray-800/40 dark:text-white/70">
           <GoDotFill className="w-20 h-20 animate-bounce [animation-delay:-0.3s]" />
           <GoDotFill className="w-20 h-20 animate-bounce [animation-delay:-0.15s]" />
           <GoDotFill className="w-20 h-20 animate-bounce" />
