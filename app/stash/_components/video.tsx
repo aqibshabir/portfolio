560542361329cdecfa947df5dd2ece953e2b66fa
aqibshabir <aqibfaisalshabir@gmail.com>
2024-06-16 16:48:09 +0100

    sorted the video component with a loading state

--- app/stash/_components/video.tsx
+++ app/stash/_components/video.tsx
@@ -2,13 +2,15 @@ import React from "react";
 
 function Video() {
   return (
-    <div className="md:mb-10">
-      <iframe
-        className="h-[350px] sm:h-[440px] md:h-[570px] lg:h-[724px]"
-        src="https://player.vimeo.com/video/959160916?h=c5da17696c&background=1&loop=1&autoplay=1&mute=1"
-        width="100%"
-        allowFullScreen
-      ></iframe>
+    <div className="mb-10 flex justify-center items-center h-[220px] xs:h-[270px] sm:h-[340px] md:h-[410px] lg:h-[576px] xl:h-[740px]">
+      <div className="bg-black dark:bg-transparent/20 absolute top-[4%] w-[350px] h-[197px] xs:w-[500px] xs:h-[281px] sm:w-[640px] sm:h-[360px] md:w-[768px] md:h-[432px] lg:w-[1024px] lg:h-[576px] xl:w-[1290px] xl:h-[726px]">
+        <iframe
+          className="top-[4%] w-full h-full"
+          src="https://player.vimeo.com/video/959160916?h=c5da17696c&background=1&loop=1&autoplay=1&mute=1"
+          width="100%"
+          allowFullScreen
+        ></iframe>
+      </div>
     </div>
   );
 }
