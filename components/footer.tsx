a2ffbb3201da7c72583d4dedae5e6f9015b3645c
Aqib Shabir <aqibfaisalshabir@gmail.com>
2025-01-05 20:56:33 +0000

    automatically updating year using Date object

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,14 +1,13 @@
-import React from "react";
+import React from 'react';
 
 export default function Footer() {
   return (
     <footer className="mb-10 px-4 text-center text-gray-500 dark:text-white">
       <small className="mb-1 text-xs block">
-        &copy; 2024 Aqib Shabir. All rights reserved
+        &copy; {new Date().getFullYear()} Aqib Shabir. All rights reserved
       </small>
       <p className="text-xs mb-1 font-thin">
-        Made with: React, Nextjs, Typescript, Tailwind CSS, Framer Motion, React
-        Email &amp; Resend.
+        Made with: React, Nextjs, Typescript, Tailwind CSS, Framer Motion, React Email &amp; Resend.
       </p>
       <p className="text-xs font-thin">Deployed on Vercel.</p>
     </footer>
